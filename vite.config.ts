34d8806c358937628d094e74417ccb4a85c1d49b
pavlo-zinchenko <pavlozinchenko.pz@gmail.com>
2024-10-25 19:40:01 +0200

    Update aliases and config files

--- vite.config.ts
+++ vite.config.ts
@@ -9,13 +9,14 @@ export default defineConfig({
 			'@assets': path.resolve(__dirname, './src/assets'),
 			'@common': path.resolve(__dirname, './src/components/common'),
 			'@components': path.resolve(__dirname, './src/components'),
-			'@constants': path.resolve(__dirname, './src/constants'),
+			'@constants': path.resolve(__dirname, './src/constants/index.ts'),
+			'@interfaces': path.resolve(__dirname, './src/interfaces/index.ts'),
 			'@pages': path.resolve(__dirname, './src/pages'),
 			'@services': path.resolve(__dirname, './src/services'),
 			'@store': path.resolve(__dirname, './src/store'),
 			'@slices': path.resolve(__dirname, './src/store/slices'),
 			'@utils': path.resolve(__dirname, './src/utils'),
-			'@validations': path.resolve(__dirname, './src/validations')
+			'@validations': path.resolve(__dirname, './src/validations'),
 		}
 	}
 });
